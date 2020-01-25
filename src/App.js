9fe4e75c73cc1666b929c9a9b78c3a453fe45a4f
sahiltgi <abhi28005@gmail.com>
2020-01-25 21:24:37 +0530

    header add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import MenuBar from "./components/MenuBar";
+import Screen from "./components/FrontScreen";
 
 function App() {
   return (
     <div className="App">
+      <Screen />
       <MenuBar />
     </div>
   );
---
+++ src/components/Heading/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="header">
+        <h1 className="text-header">News Feeds</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
