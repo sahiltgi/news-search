e083389cdae54d5763b2134915ff1e5ab1f36068
sahiltgi <abhi28005@gmail.com>
2020-01-25 19:12:46 +0530

    css of nav

--- src/components/MenuBar/index.jsx
+++ src/components/MenuBar/index.jsx
@@ -8,9 +8,9 @@ class MenuBar extends Component {
   render() {
     return (
       <div className="SideMenu">
-        <Menu listItem={NEWS_COUNTRY} listName={"Country Name"} />
-        <Menu listItem={NEWS_CATEGORIES} listName={"News Categories"} />
-        <Menu listItem={NEWS_LANGUAGES} listName={"News Languages"} />
+        <Menu listItem={NEWS_COUNTRY} listName={"Select Country"} />
+        <Menu listItem={NEWS_CATEGORIES} listName={"News Category"} />
+        <Menu listItem={NEWS_LANGUAGES} listName={"News Language"} />
       </div>
     );
   }
